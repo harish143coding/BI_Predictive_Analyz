0130fabc484db7f32b4f4bf6a8da0d67170cd58d
harish143coding <harish.moturu501@gmail.com>
2024-10-23 15:43:29 +0200

    Instance: created an engine obeject for SQLAlchemy

--- Job_analysis.py
+++ Job_analysis.py
@@ -4,18 +4,41 @@ step 1: create a db and load the existing data into the db
 step 2 : fetch this data from the db to find KPIs
 
 """
+from sqlalchemy import URL
+from sqlalchemy import create_engine
 import pandas as pd
 
-positions_df = pd.read_excel(io="../applications-flow.xlsx"
-                             )
-print(positions_df.shape)
-print(positions_df.head)
+positions_df = pd.read_excel(io="../applications-flow.xlsx")
+
+positions_df.shape
+positions_df.head
 
 # pandas remaining columns
 positions_df.rename(mapper={"Date": "date of apply", "Position": "Job_role",
                             "company": "company", "Link": "details", "Response": "response"},
-                    axis='columns', inplace=True)
+                    axis='columns',
+                    inplace=True)
 
-print(positions_df.columns)
 # original dataframe is modified by removing the null columns
 modified_df = positions_df.loc[:, 'date of apply': 'response']
+print(modified_df.describe())
+
+# insert this dataframe into a table by creating a new table in MS SQL server.
+# -- creating the engine to connect to the database
+url_object = URL.create(
+    "mssql+pyodbc",
+    username="DESKTOP-PT5IK6F/harish",
+    host="DESKTOP-PT5IK6F",
+    database="sampledb"
+     )
+# creating engine string in the following method 1
+engine_1 = create_engine(url_object)
+
+# creating engine string in the following method 2
+engine_2 = create_engine("mssql+pyodbc://DESKTOP-PT5IK6F/harish:@localhost/sampledb",
+                         pool_pre_ping= True
+                        )
+
+# Check the connection
+with engine_2.connect() as connection:
+    print("Connection successful!")
