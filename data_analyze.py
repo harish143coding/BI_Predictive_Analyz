faf527a7994c1b9b5afe561972db9ed4e9f731d6
harish143coding <harish.moturu501@gmail.com>
2024-11-06 17:48:32 +0100

    continue working on company name

--- data_analyze.py
+++ data_analyze.py
@@ -18,19 +18,23 @@ df = pd.read_sql('Apply', db_engine)
 
 # Data Cleaning/Enhancing: improve the quality of the data in the frame
 # replacing the 'None' values in between the timestamps with preceding time stamp.
-df['date of apply'].fillna('ffill', inplace=True)
+df['date of apply'].fillna('ffill')
 
 # finding the company location and adding that to new column
 
+
 def get_company_location(company_name):
-    url = "https://handelsregister-api.de/api/companies"
-    params = {"company_name": company_name}
-    response = requests.get(url, params=params)
+    url = "https://search.infobelpro.com/germany/en/api/search"
+    headers = {"Content-Type": "application/json"}
+    payload = {"company_name": company_name}
+    response = requests.post(url, headers=headers, json=payload)
     if response.status_code == 200:
         return response.json()["location"]
     else:
-        return None
+        return "Location not found"
 
-df['Location'] = df['Company Name'].apply(get_company_location)
 
-# implement the above function and check.
+df['Location_1'] = df['company'].apply(get_company_location)
+print(df.head())
+print(df.info())
+# implement the above function and check... shoud be worked on further.
