61b1a7ac34a58c8ec322a6ddcc8b460cb4c268ca
harish143coding <harish.moturu501@gmail.com>
2024-11-15 16:09:18 +0100

    Open GoV anaysis on Employment

---
+++ OGD_Employment_analysis.py
@@ -0,0 +1,38 @@
+"""
+Brainstorming:
+On which data from the Open Govt Data portal should be worked on?
+1. https://data.gov.in/
+2. There datasets classified under 36 public departments
+3. for the first choosing the department of Labour and employment.
+  - for the first analysis data from the follwing link be used
+  "https://www.data.gov.in/resource/state-wise-jobseeker-registration-ncs-portal-till-30-june-2022"
+"""
+
+from config import API_INFO
+import requests
+
+# Define the API endpoint and your API key
+api_url = "https://api.data.gov.in/resource/a79d3456-edcc-4c52-9be8-7682476cb64a?"
+api_key = API_INFO["Employment_api_key"]
+format_type = "json"
+
+params = { "api-key": api_key,
+           "format": "json" }
+
+# Set the headers including the API key
+headers = {
+    "Authorization": f"Bearer {api_key}",
+    "Accept": f"application/{format_type}"
+}
+
+# Make the GET request to the API with headers
+response = requests.get(api_url, params=params)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse the JSON response
+    data = response.json()
+    # Print the retrieved data
+    print(data)
+else:
+    print(f"Failed to retrieve data: {response.status_code}")
