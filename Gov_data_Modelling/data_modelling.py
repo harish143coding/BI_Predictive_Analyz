5dbe2e64cd533ae5862594cedbda56b8d88e3a57
harish143coding <harish.moturu501@gmail.com>
2024-12-03 16:09:06 +0100

    data accessed from the weather API next step data understanding

--- Gov_data_Modelling/data_modelling.py
+++ Gov_data_Modelling/data_modelling.py
@@ -9,34 +9,43 @@ https://www.data.gov.in/resource/daily-rainfall-data-national-remote-sensing-cen
 """
 from config import API_INFO
 import requests
+import pandas as pd
 
 
 # define the API endpoint and your API key. here weather data
 weather_api_endpoint = "https://api.data.gov.in/resource/b3521980-43d8-4d22-b86e-43f9a927f4b9?"
 
+
+
 parameters = {
-                "api_key": API_INFO["weather_api_key"],
+                "api-key": API_INFO["weather_api_key"],
                 "format": "json",
-                "limit": 1000
+               "limit": 10000  # for scaling the database how to retrieve the further records
 }
 
 # Make the GET request to the API with parameters
 response = requests.get(weather_api_endpoint, params=parameters)
-
 
 # Check if the request was successful
 if response.status_code == 200:
     # Parse the JSON response
     data = response.json()
     # Print the retrieved data
-    print(type(data))
+    print(type(response))
 else:
     print(f"Failed to retrieve data: {response.status_code}")
+
+
+print(len(data['records']))
+df = pd.DataFrame(data['records'])
+print(df.head())
+print(df.info())
+print(df.groupby(["_state_"]).mean("avg_rainfall"))
+
 
 
 """
 Next steps:
 test the API:
-find the error
-may be error in api_key or URL?
+go through the rainfall data from the dataframe it looks like the same avg rainfall in all the states
 """
