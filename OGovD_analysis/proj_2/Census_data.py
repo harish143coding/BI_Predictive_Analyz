3c468397bf5a5eee22ffaae2abe27b9f504a0b6f
harish143coding <harish.moturu501@gmail.com>
2024-12-05 17:04:57 +0100

    data understanding and calculation of baisc KPIs

--- OGovD_analysis/proj_2/Census_data.py
+++ OGovD_analysis/proj_2/Census_data.py
@@ -11,12 +11,40 @@ from config import API_INFO
 census_url = "https://api.data.gov.in/resource/7360817d-7c02-4e0d-9143-976741df656e?"
 
 params = {
-    "api-key": API_INFO["api_key"]
+    "api-key": API_INFO["api_key"],
+    "format": "json",
+    "limit": 100000
 }
+
+response = requests.get(census_url, params=params)
+data = response.json()
+print(type(data))
+
+df = pd.DataFrame(data["records"])
+print(df.head())
+print(df['total_population_person'].unique())
+
+# In data understanding it is noticed that few of the metric coloumns are non-numeric so need perform data cleaning
+""" Data Cleaning: converting the non numeric columns to numeric and deleting the rows consisting all zero columns"""
+# Convert all columns to numeric where possible
+df = df.apply(pd.to_numeric, errors='coerce')
+
+# Drop rows where all metric values are zero (assuming 'state_uts_code' and 'district_code' are non-metric)
+metric_columns = ['total_population_person', 'area']
+df = df[~(df[metric_columns].fillna(0).eq(0).all(axis=1))]
+
+
+# Performing some basic descriptive analysis
+def calculate_basic_stats(data: pd.DataFrame):
+    total_population = df.groupby(["state_uts_code", "district_code"]).agg(
+        {"total_population_person": "sum"})
+    avg_popuation = df.groupby(["state_uts_code", "district_code"])["total_population_person"].mean().reset_index()
+    return total_population, avg_popuation
+
+calculate_basic_stats(df)
 
 
 """
 next steps:
-retrieve the data from this API and make data understanding
+During data understanding found some anamolies try correct them and finish the descriptive analysis
 """
-
