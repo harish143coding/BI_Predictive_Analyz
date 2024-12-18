b19695dbf1dbd2b65efbd03f253b4acf2a5532e1
harish143coding <harish.moturu501@gmail.com>
2024-12-18 15:28:44 +0100

    brainstorming towards the mausam dwh

---
+++ Gov_data_Modelling/weather_data_extension/weather_dwh.py
@@ -0,0 +1,19 @@
+"""
+GOAL: BHARAT MAUSAM ki DATAWAREHOUSE
+In weather data model package a tiny dwh for the weather data i.e. rainfall from 2023 November  is loaded
+Idea -> how to scale the warehouse with further rainfall data
+Brainstorming
+1. Dataset from API: https://api.data.gov.in/catalog/a6007b2f-eed3-4a68-a321-d2d563d52bb2? this API consists of avg_rainfall
+   data for all the states approx until 2023. (site: https://www.data.gov.in/apis/a6007b2f-eed3-4a68-a321-d2d563d52bb2)
+2. how can I get statewise temperature historical data, so that fact data contains values of monthly temperature state-wise?
+"""
+
+
+
+
+
+"""
+next steps:
+reusing the configs for API and DB how?
+find the resource for temperatures monthly state wise
+"""
