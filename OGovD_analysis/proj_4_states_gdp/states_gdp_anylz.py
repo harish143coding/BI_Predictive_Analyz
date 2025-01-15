63e1f60093f63b8ed673a43c15fa2e300240c389
harish143coding <harish.moturu501@gmail.com>
2025-01-15 15:58:19 +0100

    new analysis on GSDP

---
+++ OGovD_analysis/proj_4_states_gdp/states_gdp_anylz.py
@@ -0,0 +1,14 @@
+"""
+the idea is to carry-out appropriate visualization preferably Geo-Visualization if no the other
+Dataset (Gross State Domestic Product): https://www.data.gov.in/resource/gross-state-domestic-product-gsdp-current-prices-states-and-uts-2011-12-2021-22
+"""
+
+
+
+
+
+
+"""
+Next steps:
+ingest the data.
+"""
