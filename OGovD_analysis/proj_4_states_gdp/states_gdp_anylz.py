aaca52fd938afaf725c506ec10203e6f3723cff8
harish143coding <harish.moturu501@gmail.com>
2025-01-16 16:58:04 +0100

    correcting the package structure

--- OGovD_analysis/proj_4_states_gdp/states_gdp_anylz.py
+++ OGovD_analysis/proj_4_states_gdp/states_gdp_anylz.py
@@ -2,7 +2,21 @@
 the idea is to carry-out appropriate visualization preferably Geo-Visualization if no the other
 Dataset (Gross State Domestic Product): https://www.data.gov.in/resource/gross-state-domestic-product-gsdp-current-prices-states-and-uts-2011-12-2021-22
 """
+from OGovD_analysis.config import API_INFO
+import requests
 
+
+gsdp_url = "https://api.data.gov.in/resource/adb4b1da-159f-46b3-a9c0-0545fe9ddda0?"
+
+params = {
+    "api-key": API_INFO["api_key"],
+    "format": "json",
+    "limit": 10000
+}
+
+response = requests.get(gsdp_url, params=params)
+data = response.json()
+print(data["total"])
 
 
 
@@ -10,5 +24,5 @@ Dataset (Gross State Domestic Product): https://www.data.gov.in/resource/gross-s
 
 """
 Next steps:
-ingest the data.
+work on the import error from Chat* soutions
 """
